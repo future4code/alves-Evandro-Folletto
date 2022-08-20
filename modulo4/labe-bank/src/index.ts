a6a046200b12a75b51920f7c3651fd75f3342ee4
Evandro Paulo Folletto <evandrofolletto@gmail.com>
2022-08-19 23:43:31 -0300

    labe-bank exercícios e desafios finalizados

--- modulo4/labe-bank/src/index.ts
+++ modulo4/labe-bank/src/index.ts
@@ -7,9 +7,10 @@ app.use(express.json());
 app.use(cors());
 
 type transaction = {
+  cpf: number,
   value: number,
-  date: Date,
   description: string,
+  date?: Date,
 }
 
 type userAccount = {
@@ -20,22 +21,22 @@ type userAccount = {
   extract: transaction[],
 }
 
-function calAge(dataNasc:string) {
+function calAge(dataNasc: string) {
   const dataAtual = new Date();
   const anoAtual = dataAtual.getFullYear();
-  const mesAtual = dataAtual.getMonth()+1;
+  const mesAtual = dataAtual.getMonth() + 1;
   const diaAtual = dataAtual.getDate();
   const anoNascParts = dataNasc.split('/');
   const diaNasc = Number(anoNascParts[0]);
   const mesNasc = Number(anoNascParts[1]);
   const anoNasc = Number(anoNascParts[2]);
   let idade = anoAtual - anoNasc;
-  
-  if(mesAtual < mesNasc) {
+
+  if (mesAtual < mesNasc) {
     idade--;
   } else {
-    if(mesAtual === mesNasc){
-      if(diaAtual < diaNasc) idade--
+    if (mesAtual === mesNasc) {
+      if (diaAtual < diaNasc) idade--
     }
   }
 
@@ -51,30 +52,30 @@ app.get("/accounts", (req: Request, res: Response) => {
 app.post("/account", (req: Request, res: Response) => {
   try {
     const { name, cpf, birthDate } = req.body;
-    if(!name || !cpf || !birthDate) {
+    if (!name || !cpf || !birthDate) {
       res.statusCode = 401;
       throw new Error('Existem dados faltantes!');
     }
     const idade = calAge(birthDate);
-    if(idade < 18) {
+    if (idade < 18) {
       res.statusCode = 401;
       throw new Error('É necessário ser maior de idade para criar conta!');
     }
-    if(typeof(name) !== 'string' || typeof(cpf) !== 'string' || typeof(birthDate) !== 'string') {
+    if (typeof (name) !== 'string' || typeof (cpf) !== 'string' || typeof (birthDate) !== 'string') {
       res.statusCode = 401;
       throw new Error('Os formatos de todos os dados devem ser strings!');
     }
-    const cpfExist = accounts.filter( (account:userAccount) => {
+    const cpfExist = accounts.filter((account: userAccount) => {
       return cpf === account.cpf
     })
-    if(cpfExist.length) {
+    if (cpfExist.length) {
       res.statusCode = 401;
       throw new Error('O CPF fornecido já existe!');
     }
-    const newAccount = {name, cpf, birthDate};
+    const newAccount = { name, cpf, birthDate };
     accounts.push(newAccount);
     res.status(200).send("Conta criada com sucesso!")
-  } catch (error:any) {
+  } catch (error: any) {
     res.status(res.statusCode).send({ message: error.message })
   }
 })
@@ -83,24 +84,24 @@ app.post("/account", (req: Request, res: Response) => {
 app.get("/balance", (req: Request, res: Response) => {
   try {
     const { cpf } = req.body;
-    if(!cpf) {
+    if (!cpf) {
       res.statusCode = 401;
       throw new Error('Existem dados faltantes!');
     }
-    if(typeof(cpf) !== 'string') {
+    if (typeof (cpf) !== 'string') {
       res.statusCode = 401;
       throw new Error('Os formatos de todos os dados devem ser strings!');
     }
-    const balanceAccount = accounts.filter( (account:userAccount) => {
+    const balanceAccount = accounts.filter((account: userAccount) => {
       return account.cpf === cpf
     })
-    if(!balanceAccount.length){
+    if (!balanceAccount.length) {
       res.statusCode = 401;
       throw new Error('A conta não foi encontrada!');
     } else {
       res.send(`O saldo da sua conta é R$ ${balanceAccount[0].balance}`);
     }
-  } catch (error:any) {
+  } catch (error: any) {
     res.status(res.statusCode).send({ message: error.message });
   }
 })
@@ -109,18 +110,18 @@ app.get("/balance", (req: Request, res: Response) => {
 app.put("/deposit", (req: Request, res: Response) => {
   try {
     const { name, cpf, value } = req.body;
-    if(!name || !cpf || !value) {
+    if (!name || !cpf || !value) {
       res.statusCode = 401;
       throw new Error('Existem dados faltantes!');
     }
-    if(typeof(name) !== 'string' || typeof(cpf) !== 'string' || typeof(value) !== 'number') {
+    if (typeof (name) !== 'string' || typeof (cpf) !== 'string' || typeof (value) !== 'number') {
       res.statusCode = 401;
       throw new Error('Verificar o formato dos valores da requisição!');
     }
-    accounts.map( (account:userAccount) => {
-      if(account.name === name && account.cpf === cpf){
+    accounts.map((account: userAccount) => {
+      if (account.name === name && account.cpf === cpf) {
         account.balance += value;
-        const newTransaction = {
+        const newTransaction: any = {
           value,
           date: new Date(),
           description: "Depósito de dinheiro",
@@ -129,60 +130,135 @@ app.put("/deposit", (req: Request, res: Response) => {
       }
     })
     res.status(200).send(`Depósito efetuado com sucesso!`);
-  } catch (error:any) {
+  } catch (error: any) {
     res.status(res.statusCode).send({ message: error.message });
   }
 })
 
+function stringDateInDate(vencimento: string) {
+  const vencArray = vencimento.split('/');
+
+  const aux = vencArray[0];
+  vencArray[0] = vencArray[2];
+  vencArray[2] = aux;
+  const dataString = vencArray.join("-");
+  return new Date(dataString)
+}
+
 // Pagar conta
 app.post("/pay", (req: Request, res: Response) => {
+  try {
+    let { cpf, description, value, vencimento } = req.body;
+    vencimento = vencimento ? stringDateInDate(vencimento) : new Date();
+    if (vencimento < new Date()) {
+      res.statusCode = 401;
+      throw new Error('A data não pode ser inferior a data atual!');
+    }
+    if (!cpf || !description || !value) {
+      res.statusCode = 401;
+      throw new Error('Existem dados obrigatórios faltantes!');
+    }
+    if (typeof (cpf) !== 'string' || typeof (description) !== 'string' || typeof (value) !== 'number') {
+      res.statusCode = 401;
+      throw new Error('Verificar o formato dos valores da requisição!');
+    }
+    accounts.map((account: userAccount) => {
+      if (account.cpf === cpf) {
+        if(value > account.balance){
+          res.statusCode = 401;
+          throw new Error('Saldo indisponível!');
+        }
+        const newTransaction: any = {
+          description,
+          value,
+          vencimento,
+          jaDescontado: false,
+        }
+        account.extract.push(newTransaction);
+      }
+    })
+    res.status(200).send(`Pagamento efetuado com sucesso!`);
+  } catch (error: any) {
+    res.status(res.statusCode).send({ message: error.message });
+  }
+})
 
-  const data3 = '01/02/2020';
-  const data3B = data3.split('/');
+// Atualizar saldo
+app.put("/balance", (req: Request, res: Response) => {
+  accounts.forEach((account: any) => {
+    let sumArray = 0;
+    for(let i=0 ; i<account.extract.length ; i++){
+      if(account.extract[i].description === "Transferência realizada") {
+        sumArray += account.extract[i].value;
+      } else if(account.extract[i].description === "Transferência recebida") {
+        sumArray -= account.extract[i].value;
+      } else if(account.extract[i].description !== "Depósito de dinheiro" && stringDateInDate(account.extract[i].date) < new Date() && !account.extract[i].jaDescontado){
+        sumArray += account.extract[i].value;
+        account.extract[i].jaDescontado = true;
+      }
+    }
+    account.balance -= sumArray;
+  })
+  res.status(200).send(`Saldos atualizados com sucesso!`);
+})
 
-  const aux = data3B[0];
-  data3B[0] = data3B[2];
-  data3B[2] = aux;
-  const dataString = data3B.join("");
+// Transferência entre contas
+app.post("/transfer", (req: Request, res: Response) => {
+  try {
+    let { nameSender, cpfSender, nameRecipient, cpfRecipient, value } = req.body;
+    if (!nameSender || !cpfSender || !nameRecipient || !cpfRecipient || !value) {
+      res.statusCode = 401;
+      throw new Error('Existem dados obrigatórios faltantes!');
+    }
+    if (typeof(nameSender) !== 'string' || typeof(cpfSender) !== 'string' || typeof(nameRecipient) !== 'string' || typeof(cpfRecipient) !== 'string' || typeof(value) !== 'number') {
+      res.statusCode = 401;
+      throw new Error('Verificar o formato dos valores da requisição!');
+    }
+    const accountSenderExist = accounts.filter( (account:userAccount) => {
+      return account.name === nameSender && account.cpf === cpfSender      
+    })
+    if (!accountSenderExist.length) {
+      res.statusCode = 401;
+      throw new Error('A conta do remetente não foi encontrada!');
+    }
+    const accountRecipientExist = accounts.filter( (account:userAccount) => {
+      return account.name === nameRecipient && account.cpf === cpfRecipient
+    })
+    if (!accountRecipientExist.length) {
+      res.statusCode = 401;
+      throw new Error('A conta do destinatário não foi encontrada!');
+    }
+    accounts.map((account: userAccount) => {
+      if (account.name === nameSender && account.cpf === cpfSender) {
+        if (value > account.balance){
+          res.statusCode = 401;
+          throw new Error('Saldo insuficiente para realizar a transferência!');
+        }
+        const newTransaction: any = {
+          description: "Transferência realizada",
+          value,
+          date: new Date(),
+          jaDescontado: false,
+        }
+        account.extract.push(newTransaction);
+      }
+    })
+    accounts.map((account: userAccount) => {
+      if (account.name === nameRecipient && account.cpf === cpfRecipient) {
+        const newTransaction: any = {
+          description: "Transferência recebida",
+          value,
+          date: new Date(),
+          jaDescontado: false,
+        }
+        account.extract.push(newTransaction);
+      }
+    })
 
-  console.log(dataString);
-  res.send(dataString);
-
-  const data0 = new Date();
-  // const data1 = new Date('2000-01-01');
-  const data1 = new Date('11/20/2010');
-  const data2 = new Date('02/04/2210');
-  // const data2 = '01/01/2022';
-  
-  const dataAgora = new Date();
-  
-  // res.send(data0>data1);
-
-  // try {
-  //   const { cpf, description, value, vencimento } = req.body;
-  //   if(!cpf || !description || !value || !vencimento) {
-  //     res.statusCode = 401;
-  //     throw new Error('Existem dados faltantes!');
-  //   }
-  //   if(typeof(cpf) !== 'string' || typeof(value) !== 'number') {
-  //     res.statusCode = 401;
-  //     throw new Error('Verificar o formato dos valores da requisição!');
-  //   }
-  //   accounts.map( (account:userAccount) => {
-  //     if(account.name === name && account.cpf === cpf){
-  //       account.balance += value;
-  //       const newTransaction = {
-  //         value,
-  //         date: new Date(),
-  //         description: "Depósito de dinheiro",
-  //       }
-  //       account.extract.push(newTransaction);
-  //     }
-  //   })
-  //   res.status(200).send(`Depósito efetuado com sucesso!`);
-  // } catch (error:any) {
-  //   res.status(res.statusCode).send({ message: error.message });
-  // }
+    res.status(200).send(`Pagamento efetuado com sucesso!`);
+  } catch (error: any) {
+    res.status(res.statusCode).send({ message: error.message });
+  }
 })
 
 app.listen(3003, () => {
