38dab9e58947978cbe1a89ba0a983c07ae1de7dc
Evandro Paulo Folletto <evandrofolletto@gmail.com>
2022-09-12 15:19:20 -0300

    exercício 5 pronto

---
+++ modulo5/introducao-autenticacao/src/data/selectUserByEmail.ts
@@ -0,0 +1,11 @@
+import connection from './connection';
+
+export default async (email: string): Promise<any> => {
+  const [user] = await connection.raw(`
+    SELECT *
+    FROM 20220912_User
+    WHERE (20220912_User.email = email)
+  `)
+
+  return user
+};
