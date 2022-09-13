fbcda447ea3aee626ea99e71ec9bb6ad8cb60814
Evandro Paulo Folletto <evandrofolletto@gmail.com>
2022-09-13 20:53:12 -0300

    Exercício 4 pronto

--- modulo5/criptografia-e-user-roles/src/endpoints/getUser.ts
+++ modulo5/criptografia-e-user-roles/src/endpoints/getUser.ts
@@ -9,6 +9,11 @@ export default async (req: Request, res: Response): Promise<any> => {
     const dados = verifyToken(token);
 
     const user = await selectUserById(dados.id);
+    console.log(user)
+
+    if(user[0].role !== "NORMAL"){
+      throw new Error("Unauthorized");
+    }
 
     if(user.length){
       res.status(200).send({
