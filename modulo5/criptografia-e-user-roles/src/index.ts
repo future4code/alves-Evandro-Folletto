495f4b9fa1742d92f1a7aded17b5e34a15959cc1
Evandro Paulo Folletto <evandrofolletto@gmail.com>
2022-09-13 21:54:04 -0300

    Desafio - Exercício 6 pronto

--- modulo5/criptografia-e-user-roles/src/index.ts
+++ modulo5/criptografia-e-user-roles/src/index.ts
@@ -3,8 +3,10 @@ import createUser from "./endpoints/createUser";
 import login from "./endpoints/login";
 import getUser from "./endpoints/getUser";
 import deleteUser from "./endpoints/deleteUser";
+import getUserById from "./endpoints/getUserById";
 
 app.post("/user/signup", createUser)
 app.post("/user/login", login)
 app.get("/user/profile", getUser)
 app.delete("/user/:id", deleteUser)
+app.get("/user", getUserById)
