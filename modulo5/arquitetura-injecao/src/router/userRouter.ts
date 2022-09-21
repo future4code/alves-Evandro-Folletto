6685eedff518ee97d10c496fc753f3f199016592
Evandro Paulo Folletto <evandrofolletto@gmail.com>
2022-09-21 14:10:30 -0300

    exercício 3 - pronto

--- modulo5/arquitetura-injecao/src/router/userRouter.ts
+++ modulo5/arquitetura-injecao/src/router/userRouter.ts
@@ -1,9 +1,21 @@
 import { Router } from 'express';
 import UserController from './../controller/UserController';
+import UserBusiness from './../business/UserBusiness';
+import UserDatabase from './../database/UserDatabase';
+import { IdGenerator } from './../services/IdGenerator';
+import { HashManager } from './../services/HashManager';
+import { Authenticator } from './../services/Authenticator';
 
 export const userRouter = Router();
 
-const userController = new UserController();
+const userController = new UserController(
+  new UserBusiness(
+    new UserDatabase(),
+    new IdGenerator(),
+    new HashManager(),
+    new Authenticator()
+  )
+);
 
 userRouter.post("/signup", userController.signup);
 userRouter.put("/edit/:id", userController.edit);
